e04cafa5ff119389f27604bf20bb3019e79b3ed6
user <user@DESKTOP-1PVN1U3>
2020-06-04 17:48:27 +0900

    [edit] file

--- BlackJack/src/gameObject/Card.java
+++ BlackJack/src/gameObject/Card.java
@@ -1,5 +1,9 @@
 package gameObject;
 
 public class Card {
+	//멤버 변수
+	int cardNum; // 1 ~ 13
+	int cardPattern; // 카드 무늬
+	
 
 }
--- BlackJack/src/gameObject/CardDeck.java
+++ BlackJack/src/gameObject/CardDeck.java
@@ -1,5 +1,6 @@
 package gameObject;
 
 public class CardDeck {
-
+	int rNum[] = new int[52]; // 랜덤 번호
+	boolean rSwitch[] = new boolean[52]; // 중복 체크
 }
